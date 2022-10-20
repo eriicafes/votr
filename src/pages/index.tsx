f36cf9b3a1a51062f3552865d7bcdcab501ae907
Eric Afes <eriicafes@gmail.com>
2022-10-20 14:47:53 +0100

    Add stacks auth with react query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
+import { useUserData, useConnect, useDisconnect } from "@/stacks/auth"
+
 export default function Home() {
+  const user = useUserData()
+  const connect = useConnect()
+  const disconnect = useDisconnect()
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <p className="font-bold text-xl">Hello Next</p>
+    <div className="flex flex-col items-center justify-center gap-4 min-h-screen">
+      <p className="font-bold text-xl">Hello Next Stacks</p>
+
+      {user.isLoading ? (
+        <p>Loading...</p>
+      ) : user.isSuccess ? (
+        <>
+          <div className="flex flex-col items-center gap-1">
+            <p className="text-sm text-gray-600">stxAddress</p>
+            <p>{user.data.profile.stxAddress.testnet}</p>
+          </div>
+          <button
+            className="bg-blue-600 text-white px-4 py-1.5 rounded-lg"
+            onClick={() => disconnect.mutate()}
+          >
+            Disconnect
+          </button>
+        </>
+      ) : (
+        <>
+          <p>{(user.error as any).message}</p>
+          <button
+            className="bg-blue-600 text-white px-4 py-1.5 rounded-lg"
+            onClick={() => connect.mutate()}
+          >
+            Connect
+          </button>
+        </>
+      )}
     </div>
   )
 }
---
+++ src/stacks/auth.ts
@@ -0,0 +1,50 @@
+import { AppConfig, FinishedAuthData, showConnect, UserSession } from "@stacks/connect";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+
+export const userSession = new UserSession({
+    appConfig: new AppConfig(["store_write", "publish_data"])
+})
+
+export function loadUserData() {
+    return userSession.loadUserData()
+}
+
+export async function connect() {
+    return new Promise<FinishedAuthData>((resolve, reject) => {
+        showConnect({
+            appDetails: {
+                name: "Next Stacks",
+                icon: window.location.origin + "/vercel.svg",
+            },
+            onFinish: resolve,
+            onCancel: reject,
+            userSession,
+        });
+    })
+}
+
+export async function disconnect() {
+    return userSession.signUserOut()
+}
+
+export const useUserData = () => useQuery(["stacks"], loadUserData, { retry: false })
+
+export const useConnect = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation(connect, {
+        onSuccess() {
+            return queryClient.invalidateQueries(["stacks"])
+        }
+    })
+}
+
+export const useDisconnect = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation(disconnect, {
+        onSuccess() {
+            return queryClient.invalidateQueries(["stacks"])
+        }
+    })
+}
